ca88ccd3fd693da30b4fbb7b2939ac1d95103f72
Shbak <khbak111@naver.com>
2023-07-30 14:55:02 +0900

    카카오맵 지도추가 -> "주소/map"으로 접근가능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Home from "./routes/Home";
 import Second from "./routes/Second";
-
+import Map from "./routes/Map";
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/map">
+          <Map />
+        </Route>
         <Route path="/2">
           <Second />
         </Route>
---
+++ src/routes/Map.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import MapContainer from "../componentes/MapContainer";
+function Map() {
+  return (
+    <div>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default Map;
