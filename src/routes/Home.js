7457238ad2e36bc4d6a95d2227492935b7e3f824
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-08-03 17:57:19 +0900

    화면이동

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,32 @@
-function Home() {
-  return <div>Hello im Home</div>;
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Show from "./routes/Show";
+import Map from "./routes/Map";
+import Restaurant_recommendation from "./routes/Restaurant_recommendation";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/restaurant_recommendation">
+          <Restaurant_recommendation />
+        </Route>
+        <Route path="/map">
+          <Map />
+        </Route>
+        <Route path="/Detail">
+          <Detail />
+        </Route>
+        <Route path="/Show">
+          <Show />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
-export default Home;
+
+export default App;
