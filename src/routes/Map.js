e35b4ce1a71d351292c196a4a357ee91d21e8a0b
Shbak <khbak111@naver.com>
2023-08-03 15:19:57 +0900

    카카오맵 api상 검색기능 추가, 화면 나누기 css 추가

--- src/routes/Map.js
+++ src/routes/Map.js
@@ -1,9 +1,26 @@
-import React, { useEffect } from "react";
-import MapContainer from "../componentes/MapContainer";
+import React, { useEffect, useState } from "react";
+import MapContainer from "../componentes/MapComponentes/MapContainer";
+import styles from "../css/Map.module.css";
+
 function Map() {
+  const [val, setVal] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => {
+    setVal(event.target.value);
+  };
+  const onClick = () => {
+    setKeyword(val);
+  };
   return (
-    <div>
-      <MapContainer />
+    <div className={styles.container}>
+      <div style={{ flex: 0.5 }}>
+        여긴 이제 좀더 세부사항 보여주는거 나오는곳
+      </div>
+      <div style={{ flex: 1 }}>
+        <input onChange={onChange} value={val} placeholder="검색" />
+        <button onClick={onClick}>검색</button>
+        <MapContainer keyword={keyword} />
+      </div>
     </div>
   );
 }
