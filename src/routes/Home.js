ff874960a16d7a119d17ec6005caa716c7afc00e
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-08-03 17:58:00 +0900

    화면이동

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,32 +1,30 @@
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import Home from "./routes/Home";
-import Detail from "./routes/Detail";
-import Show from "./routes/Show";
-import Map from "./routes/Map";
-import Restaurant_recommendation from "./routes/Restaurant_recommendation";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function App() {
+const Home = () => {
+  let history = useHistory();
+
+  const handleButtonClick1 = () => {
+    history.push('/Map');
+  };
+  const handleButtonClick2 = () => {
+    history.push('/Detail');
+  };
+  const handleButtonClick3 = () => {
+    history.push('/Show');
+  };
+  const handleButtonClick4 = () => {
+    history.push('/Restaurant_recommendation');
+  };
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/restaurant_recommendation">
-          <Restaurant_recommendation />
-        </Route>
-        <Route path="/map">
-          <Map />
-        </Route>
-        <Route path="/Detail">
-          <Detail />
-        </Route>
-        <Route path="/Show">
-          <Show />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+      <div>
+        <p>Home</p>
+        <button onClick={handleButtonClick1}>지도이동</button>
+        <button onClick={handleButtonClick2}>상세페이지이동</button>
+        <button onClick={handleButtonClick3}>공연추천이동</button>
+        <button onClick={handleButtonClick4}>관광지,먹거리이동</button>
+      </div>
   );
-}
-
-export default App;
+};
+export default Home;
