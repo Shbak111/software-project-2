2dfd55f2f685720d64f5f3101287714417ebdc7e
Shbak <khbak111@naver.com>
2023-08-10 14:00:43 +0900

    infinite-scroll 추가

--- src/componentes/MapComponentes/MapDetail.js
+++ src/componentes/MapComponentes/MapDetail.js
@@ -1,8 +1,8 @@
-function MapDetail() {
+function MapDetail({ data, index }) {
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: 30 }}>
       <img src="https://via.placeholder.com/200" alt="placeholder"></img>
-      <text style={{ padding: 10 }}>맵 요소 디테일 표시 </text>
+      <p style={{ padding: 10 }}>맵 요소 디테일 표시 {index}</p>
     </div>
   );
 }
--- src/componentes/MapComponentes/ScrollDetail.js
+++ src/componentes/MapComponentes/ScrollDetail.js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import MapDetail from "./MapDetail";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function ScrollDetail() {
+  const [items, setItems] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+
+  const fetchData = () => {
+    const newitems = [];
+    for (var i = 0; i < 10; i++) {
+      newitems.push(i);
+    }
+
+    if (items.length >= 50) {
+      sethasMore(false);
+    }
+    setItems([...items, ...newitems]);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div
+      id="scrollableDiv"
       style={{
         display: "flex",
         flexDirection: "column",
@@ -10,11 +31,30 @@ function ScrollDetail() {
         overflow: "auto",
       }}
     >
-      <MapDetail />
-      <MapDetail />
-      <MapDetail />
-      <MapDetail />
-      <MapDetail />
+      <InfiniteScroll
+        dataLength={items.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        scrollableTarget="scrollableDiv"
+      >
+        {items.length !== 0 ? (
+          items.map((item, index) => {
+            return (
+              <div key={index}>
+                <MapDetail index={index} data={item} />
+              </div>
+            );
+          })
+        ) : (
+          <div>no data</div>
+        )}
+      </InfiniteScroll>
     </div>
   );
 }
