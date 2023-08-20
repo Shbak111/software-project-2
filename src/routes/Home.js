e344a3202033fc07ce97d392336fea590153c47b
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-08-20 16:12:40 +0900

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,33 +1,64 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import { useHistory } from 'react-router-dom';
+import '../css/Home.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Select from 'react-select';
+import axios from 'axios';
+//import img from '/image1.jpg'
 
 const Home = () => {
-  let history = useHistory();
+   
 
-  const handleButtonClick1 = () => {
-    history.push("/Map");
+  const images = [
+    './image/image1.jpg',
+    './image/image2.jpg',
+    './image/image3.jpg',
+    
+  ];
+
+  const settings = {
+    dots: true,
+    dotsClass: 'slick-dots',
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // 자동 슬라이드 활성화
+    autoplaySpeed: 1700, // 1초마다 슬라이드 전환
   };
-  const handleButtonClick2 = () => {
-    history.push("/Detail");
-  };
-  const handleButtonClick3 = () => {
-    history.push("/Show");
-  };
-  const handleButtonClick4 = () => {
-    history.push("/Restaurant_recommendation");
-  };
-  const handleButtonClick5 = () => {
-    history.push("/data");
-  };
+
+  const options=[
+    { label : "부산"},
+    { label : "서울"},
+    { label : "대전"}
+  ]
+  // const [selectedOption, setSelectedOption] = useState(null);
+  // const hadleOptionChange=(option)=>{
+  //   setSelectedOption(option);
+  // }
+  
   return (
-    <div>
-      <p>Home</p>
-      <button onClick={handleButtonClick1}>지도이동</button>
-      <button onClick={handleButtonClick2}>상세페이지이동</button>
-      <button onClick={handleButtonClick3}>공연추천이동</button>
-      <button onClick={handleButtonClick4}>관광지,먹거리이동</button>
-      <button onClick={handleButtonClick5}>데이터 테스트</button>
+    <div className='container'>
+      
+      <div className="side-box">
+        <p>빠른검색</p>
+        
+        <Select defaultValue="지역" isClearable={false} isSearchable={false}  options={options}/>
+        <button >검색</button>
+      </div>
+      <div className="empty-box">
+        <Slider {...settings}>
+          {images.map((images, index) => (
+            <div key={index} style={{ display: 'flex', justifyContent: 'center' }}>
+              <img src={images} alt={`${index}`} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
+
 export default Home;
