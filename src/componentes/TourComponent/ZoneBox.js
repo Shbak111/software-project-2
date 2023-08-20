cea128e74ab2dbd9a288d8179a9530215799e538
JEONG SOEE <jj.soxee@gmail.com>
2023-08-20 16:22:03 +0900

    feat: Add SearchBar component

---
+++ src/componentes/TourComponent/ZoneBox.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ZoneContent from "./ZoneContent";
+import ZoneDetail from "./ZoneDetail";
+
+function ZoneBox() {
+  const [items, setItems] = useState([]);
+  const [selectedZone, setSelectedZone] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchData = () => {
+    const newItems = Array.from({ length: 10 }, (_, index) => index + 1);
+
+    if (items.length >= 10) {
+      setHasMore(false);
+    }
+
+    setItems([...items, ...newItems]);
+  };
+
+  // Load initial data when the component mounts
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleZoneClick = (index) => {
+    setSelectedZone(index); // Set selected zone index
+  };
+
+  return (
+    <div>
+      <h1>!!!!!!!  css 수정하기  !!!!!!!</h1>
+      {selectedZone !== null ? (
+        // If a zone is selected, show ZoneDetail
+        <ZoneDetail zoneIndex={selectedZone} />
+      ) : (
+        // Otherwise, show ZoneContent list
+        <div>
+          {items.length !== 0 ? (
+            items.map((item, index) => (
+              <div key={index} onClick={() => handleZoneClick(index)}>
+                <ZoneContent index={index} data={item} selectedZone={selectedZone} />
+              </div>
+            ))
+          ) : (
+            <div>no data</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ZoneBox;
---
+++ src/componentes/TourComponent/ZoneDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ZoneDetail({ zoneIndex }) {
+  return (
+    <div>
+      <h1>Zone Detail</h1>
+      <h1>관광지 상세페이지 추가 작성해야함.</h1>
+      <p>error: 뒤로가기 버튼시 처음화면으로 돌아감. Router 사용으로 고칠것.</p>
+      <p>Showing details for Zone index: {zoneIndex}</p>
+    </div>
+  );
+}
+
+export default ZoneDetail;
