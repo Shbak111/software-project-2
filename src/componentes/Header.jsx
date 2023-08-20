59439f1106c04da1fc030f879da80076f5be5146
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-08-20 16:11:22 +0900

    Create Header.jsx

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import '../css/Header.css';
+export default function Header (){
+    return(
+        <div className="header-container">
+            <div className="header-wrap">
+                <div className="header-left-wrap">
+                    <Link style={{display:'flex',alignitems:'center'}}className="header-Home" to="/">
+                        <p >Home</p>
+                    </Link>
+                    <ul>
+                        <li>
+                            <Link className="header-banner" to="/map">
+                                <p>map이동</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="header-banner" to="/Detail">
+                                <p>Detail이동</p>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="header-banner" to="/Restaurant_recommendation">
+                                <p>식당추천</p>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
