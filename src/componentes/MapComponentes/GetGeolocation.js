630ffb96b22a7577b1041e91a8e358dee684aff4
Shbak <khbak111@naver.com>
2023-08-22 11:00:01 +0900

    지도 디테일 클릭시 detail로 이동 추가

--- src/componentes/MapComponentes/GetGeolocation.js
+++ src/componentes/MapComponentes/GetGeolocation.js
@@ -4,7 +4,7 @@ function GetGeolocation(id) {
   var mapContainer = document.getElementById(id), // 지도를 표시할 div
     mapOption = {
       center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 10, // 지도의 확대 레벨
+      level: 7, // 지도의 확대 레벨
     };
 
   var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
--- src/componentes/MapComponentes/MapDetail.js
+++ src/componentes/MapComponentes/MapDetail.js
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
 function MapDetail({ data, index }) {
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: 30 }}>
-      <img src="https://via.placeholder.com/200" alt="placeholder"></img>
-      <p style={{ padding: 10 }}>맵 요소 디테일 표시 {index}</p>
+      <Link to="/detail">
+        <img src="https://via.placeholder.com/200" alt="placeholder"></img>
+      </Link>
+
+      <Link to="/detail">
+        <h1 style={{ padding: 10 }}>맵 요소 디테일 표시 {index}</h1>
+      </Link>
     </div>
   );
 }
