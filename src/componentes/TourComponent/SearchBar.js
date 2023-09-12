a00fae49ed3ea1190940e158b12c257091738380
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-09-12 20:58:12 +0900

    Update SearchBar.js

--- src/componentes/TourComponent/SearchBar.js
+++ src/componentes/TourComponent/SearchBar.js
@@ -1,24 +1,38 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import "./SearchBar.css"
 
 const SearchBar = ({ data  }) => {
+  const history = useHistory();
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [selectedArea, setselectedArea] = useState(null); 
+  const [selectedField, setselectedField] = useState(null);
+  const [selectedDate, setselectedDate] = useState(null);
   const handleSearch = (event) => {
     const value = event.target.value;
     setSearchTerm(value);
+    history.push({
+      pathname: '/SearchedData',
+      state: {
+        selectedArea,
+        selectedField,
+        selectedDate,
+        searchTerm
+      },
+    });
   };
-
   return (
     <div className="inputContainer">
       <div className="search">
         <input
           type="text"
-          className="inputField"
-          placeholder="검색어를 입력해주세요..."
           value={searchTerm}
-          onChange={handleSearch}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder="검색어를 입력해주세요..."
+          className="inputField"
+           
         />
+        <button onClick={handleSearch} className="search-button">검색</button>
       </div>
     </div>
   );
