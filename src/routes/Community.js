f8bee15317604ae807955ac0505ccd687b0309ee
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-09-17 17:05:25 +0900

    Community UI Update

--- src/routes/Community.js
+++ src/routes/Community.js
@@ -1,7 +1,11 @@
+import React from "react";
+import "../css/Community.css"
+import CommunityZone from "../componentes/CommunityComponent/CommunityZone"
 function Community(){
     return(
-        <div>
+        <div className="Community_container">
             <p>여긴 커뮤니티</p>
+            <CommunityZone />
         </div>
     )
 }
