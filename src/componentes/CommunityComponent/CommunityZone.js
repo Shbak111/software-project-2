201e9b39293e83105061507f7b8ef9212f6b51e1
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-09-27 01:02:07 +0900

    add: community 페이지네이션

--- src/componentes/CommunityComponent/CommunityZone.js
+++ src/componentes/CommunityComponent/CommunityZone.js
@@ -6,6 +6,14 @@ import axios from "axios";
 function CommunityZone(){
     const history=useHistory();
     const [titles,setTitles]=useState([]);
+    const [currentPage,setCurrentPage]=useState(1);
+    const [pageSize,setPageSize]=useState(10);
+    const [totalPages, setTotalPages] = useState(1);
+
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const currentItems = titles.slice(startIndex, endIndex);
+
     const handlePostClick=()=>{
         history.push("/CommunityPost");
     }
@@ -17,13 +25,32 @@ function CommunityZone(){
             method: "GET",
           });
           console.log(response.data);
-          console.log(titles._id);
           
-          const titleArray=response.data.map(item=>item.title);
+          
+          const titleArray = response.data.map(item => ({ _id: item._id, title: item.title }));
+          console.log(response.data.map(item => item._id));
           setTitles(titleArray);
+          const totalPages = Math.ceil(titleArray.length / pageSize);
+          setTotalPages(totalPages);
         }
         FetchCommunity();
-    }, []);
+    }, [pageSize]);
+
+    const renderPageButtons = () => {
+        const pageButtons = [];
+        for (let i = 1; i <= totalPages; i++) {
+          pageButtons.push(
+            <button
+              key={i}
+              onClick={() => setCurrentPage(i)}
+              disabled={currentPage === i}
+            >
+              {i}
+            </button>
+          );
+        }
+        return pageButtons;
+      };
     
     return(
         <div className="PostFrame">
@@ -35,22 +62,19 @@ function CommunityZone(){
             </div>
             <hr className="line"></hr>
             <div className="post">
-                {titles.map((title, index) => (
+                {currentItems.map((title, index) => (
                     <div className="postitem" key={index} >
                         <Link to={`/PostDetail/${index}`} style={{ display: "flex", alignItems: "center" }}>
-                            <p className="post_index" style={{marginLeft:"1%"}}>{index + 1}</p>
-                            
-                            <p className="post_title" style={{marginLeft:"12%"}}>{title}</p>
-                            
+                            <p className="post_index" style={{marginLeft:"1%"}}>{startIndex+index + 1}</p>
+                            <p className="post_title" style={{marginLeft:"12%"}}>{title.title}</p>
                             <p className="post_nickname" style={{marginLeft:"17%"}}>nickname</p>
                             <p className="post_views" style={{marginLeft:"7%"}}>0</p>
                             <hr className="post_line"/>
                         </Link>
-                        
-                        
                     </div>
                 ))}
-            </div> 
+            </div>
+            
                 
             <hr className="line"></hr>
             <div style={{display:"flex",justifyContent:"space-between"}}>
@@ -58,11 +82,19 @@ function CommunityZone(){
                     <input style={{width:"50%",marginLeft:"1%"}}></input>
                     <button>검색</button>
                 </div>
+                {/* 페이지네이션 */}
+                <div className="pagination" style={{justifyContent:"center",alignItems:"center",marginRight:"3%"}}>
+                    <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>이전 페이지</button>
+                    {renderPageButtons()}
+                    <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>다음 페이지</button>
+                </div>
+                
                 <div className="poston">
                     <button onClick={handlePostClick}>글쓰기</button>
                 </div>
 
             </div>
+            
             
         </div>
     )
