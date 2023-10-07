0f082927013d3b6bc2ca645acf132dc779798a51
gun7741 <54311686+gun7741@users.noreply.github.com>
2023-10-07 14:04:54 +0900

    add:post수정 화면이동버튼 추가

--- src/componentes/CommunityComponent/PostDetail.js
+++ src/componentes/CommunityComponent/PostDetail.js
@@ -17,6 +17,9 @@ function PostDetail() {
 
     fetchPostData();
   }, [index]);
+  const postedit=()=>{
+    history.push(`/CommunityPostUpdate/${index}`);
+  };
 
   /** 여기 코멘트 추가하는 버튼 부분 */
   const commentBtnClick = () => {
@@ -37,6 +40,7 @@ function PostDetail() {
       <p>제목: {post.title}</p>
       <p>내용: {post.content}</p>
       <p>작성자: {post.writer}</p>
+      <button onClick={postedit}>수정</button>
       <button onClick={commentBtnClick}>댓글추가</button>
     </div>
   );
