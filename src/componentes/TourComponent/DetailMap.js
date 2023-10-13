1ec8359f54d588b7bb811f392b41146b065fedf1
Shbak <khbak111@naver.com>
2023-10-13 13:49:44 +0900

    add: DetailMap ->카카오맵으로 맛집데이터 대체 방법 추가 + 해당부분 오류있음

--- src/componentes/TourComponent/DetailMap.js
+++ src/componentes/TourComponent/DetailMap.js
@@ -1,9 +1,9 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import MichelinCSV from "../../data/RB_MICHELIN_SLCTN_RSTRNT_INFO_20211231.csv";
 import michelin from "../../assets/michelin/icon.png";
 import "./DetailMap.css";
 
-function  DetailMap({ gpsX, gpsY }) {
+function DetailMap({ gpsX, gpsY }) {
   const [csvData, setCsvData] = useState([]);
   const columnIndices = {
     FCLTY_NM: 1,
@@ -11,7 +11,7 @@ function  DetailMap({ gpsX, gpsY }) {
     FCLTY_LA: 4,
     HMPG_URL: 5,
   };
-  
+
   useEffect(() => {
     const fetchAndProcessCSV = async () => {
       try {
@@ -28,48 +28,60 @@ function  DetailMap({ gpsX, gpsY }) {
   }, []);
 
   useEffect(() => {
-    if (csvData.length > 0) {
-      const { kakao } = window;
-      var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-        mapOption = {
-          center: new kakao.maps.LatLng(gpsY, gpsX), // 지도의 중심좌표
-          level: 4, // 지도의 확대 레벨
-        };
+    const { kakao } = window;
+    var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+      mapOption = {
+        center: new kakao.maps.LatLng(gpsY, gpsX), // 지도의 중심좌표
+        level: 4, // 지도의 확대 레벨
+      };
 
-      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    if (csvData.length > 0) {
       // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다
-      var imageSrc = michelin, // 마커이미지의 주소입니다    
-          imageSize = new kakao.maps.Size(25, 34.5), // 마커이미지의 크기입니다
-          imageOption = {offset: new kakao.maps.Point(13.5, 34.5)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+      var imageSrc = michelin, // 마커이미지의 주소입니다
+        imageSize = new kakao.maps.Size(25, 34.5), // 마커이미지의 크기입니다
+        imageOption = { offset: new kakao.maps.Point(13.5, 34.5) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-      var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+      var markerImage = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
       const positions = csvData.slice(1).map((row) => ({
-        content: `<div class="customoverlay">` +
-                `${row[columnIndices.HMPG_URL] === '0' ? 
-                  `<a onclick="alert('현재 존재하는 웹사이트가 없습니다.')" style="cursor: not-allowed;"><span class="michelin-title">${row[columnIndices.FCLTY_NM]}</span></a>` :
-                  `<a href="http://${row[columnIndices.HMPG_URL]}" target="_blank"><span class="michelin-title">${row[columnIndices.FCLTY_NM]}</span></a>`}` +
-                `</div>`,
+        content:
+          `<div class="customoverlay">` +
+          `${
+            row[columnIndices.HMPG_URL] === "0"
+              ? `<a onclick="alert('현재 존재하는 웹사이트가 없습니다.')" style="cursor: not-allowed;"><span class="michelin-title">${
+                  row[columnIndices.FCLTY_NM]
+                }</span></a>`
+              : `<a href="http://${
+                  row[columnIndices.HMPG_URL]
+                }" target="_blank"><span class="michelin-title">${
+                  row[columnIndices.FCLTY_NM]
+                }</span></a>`
+          }` +
+          `</div>`,
         latlng: new window.kakao.maps.LatLng(
           parseFloat(row[columnIndices.FCLTY_LA]),
           parseFloat(row[columnIndices.FCLTY_LO])
         ),
       }));
-      
-      
+
       for (let i = 0; i < positions.length; i++) {
         // 마커를 생성합니다
         const marker = new window.kakao.maps.Marker({
-          map: map,// 마커를 표시할 지도
-          position: positions[i].latlng,// 마커의 위치
+          map: map, // 마커를 표시할 지도
+          position: positions[i].latlng, // 마커의 위치
           image: markerImage, //마커 이미지 연결
         });
-        // 마커에 표시할 인포윈도우를 생성합니다 
+        // 마커에 표시할 인포윈도우를 생성합니다
         // 커스텀 오버레이를 생성합니다
         const customOverlay = new kakao.maps.CustomOverlay({
           map: map,
           position: positions[i].latlng,
           content: positions[i].content,
-          yAnchor: 1 
+          yAnchor: 1,
         });
       }
 
@@ -78,8 +90,54 @@ function  DetailMap({ gpsX, gpsY }) {
       });
       marker.setMap(map);
     }
-  }, [gpsX, gpsY,csvData]);
-  return <div id="map" style={{ height: window.innerHeight, width: window.innerWidth/2 }}></div>;
+
+    // 여기서부터는 백승훈 추가내용.
+    // 카카오맵 api categorySearch 메서드를 이용해서 음식점 데이터를 따로 쓰지 않고 지도에서 불러다 쓰는법 테스트중
+
+    // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+    // 장소 검색 객체를 생성합니다
+    var ps = new kakao.maps.services.Places(map);
+
+    // 카테고리로 음식점을 검색합니다 ("https://developers.kakao.com/docs/latest/ko/local/dev-guide#search-by-category") 여기서 코드 확인
+    ps.categorySearch("FD6", placesSearchCB, { useMapBounds: true });
+
+    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    function placesSearchCB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        for (var i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+        }
+      }
+    }
+
+    // 지도에 마커를 표시하는 함수입니다
+    function displayMarker(place) {
+      // 마커를 생성하고 지도에 표시합니다
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+
+      // 마커에 클릭이벤트를 등록합니다
+      kakao.maps.event.addListener(marker, "click", function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent(
+          '<div style="padding:5px;font-size:12px;">' +
+            place.place_name +
+            "</div>"
+        );
+        infowindow.open(map, marker);
+      });
+    }
+  }, [gpsX, gpsY, csvData]);
+  return (
+    <div
+      id="map"
+      style={{ height: window.innerHeight, width: window.innerWidth / 2 }}
+    ></div>
+  );
 }
 
 export default DetailMap;
