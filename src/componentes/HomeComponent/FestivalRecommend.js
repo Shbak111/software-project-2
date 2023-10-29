31dfc74aef67550b327170e01b85ba302bbd1046
Shbak <khbak111@naver.com>
2023-10-29 15:12:33 +0900

    fix: 홈에서 상세페이지 이동시 리뷰작성 오류 수정

--- src/componentes/HomeComponent/FestivalRecommend.js
+++ src/componentes/HomeComponent/FestivalRecommend.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './FestivalRecommend.css';
-import FetchGenreData from '../FetchGenreData';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./FestivalRecommend.css";
+import FetchGenreData from "../FetchGenreData";
 import { useSelector } from "react-redux";
 
 function FestivalRecommend() {
   const [data, setData] = useState([]);
   const [thumbnails, setThumbnails] = useState([]);
-  const initialLocation = '서울';
+  const initialLocation = "서울";
   const location = useSelector((state) => state.send.value) || initialLocation;
-  const [NoData,setNoData]=useState(false);
+  const [NoData, setNoData] = useState(false);
   useEffect(() => {
     async function fetchData(code) {
       try {
@@ -20,17 +20,21 @@ function FestivalRecommend() {
         // 두 데이터 합치기
         const fetchedData = [...dataA, ...dataB];
 
-        const filteredData = fetchedData.filter(item => {
+        const filteredData = fetchedData.filter((item) => {
           const areaElement = item.elements[6];
-          if (areaElement && areaElement.elements && areaElement.elements[0] && areaElement.elements[0].text) {
+          if (
+            areaElement &&
+            areaElement.elements &&
+            areaElement.elements[0] &&
+            areaElement.elements[0].text
+          ) {
             return areaElement.elements[0].text === location;
           }
           return false; // 'area' 요소 또는 'elements[0].text'가 없는 경우 필터링하지 않음
         });
-        if(filteredData.length===0){
+        if (filteredData.length === 0) {
           setNoData(true);
-        }
-        else{
+        } else {
           setNoData(false);
           const slicedData = filteredData.slice(0, 4);
           const thumbnailArray = slicedData.map((item) => {
@@ -41,48 +45,50 @@ function FestivalRecommend() {
           setThumbnails(thumbnailArray);
           console.log("Festival filterdata", filteredData);
         }
-        
-        
       } catch (error) {
-        console.error('Festival Error fetching data:', error);
+        console.error("Festival Error fetching data:", error);
         setNoData(true);
       }
     }
-  
+
     fetchData(["A000", "B000"]);
   }, [location]);
 
   return (
     <div className="item-box">
-      {NoData?(
+      {NoData ? (
         <p>해당 지역에 맞는 음악,미술이 없습니다.</p>
-      ):(
+      ) : (
         <div className="grid-container">
-        {thumbnails.map((image, index) => (
-          <div key={index} className="grid-item">
-            <Link to={{
-                    pathname: `/detail/${data[index]?.elements[0]?.elements[0]?.text}`,
-                    state: {
-                      title: data[index]?.elements[1]?.elements[0]?.text,
-                      place : data[index]?.elements[4]?.elements[0]?.text,
-                      realmName: data[index]?.elements[5]?.elements[0]?.text,
-                      area : data[index]?.elements[6]?.elements[0]?.text,
-                      image: data[index]?.elements[7]?.elements[0]?.text,
-                      gpsX : data[index]?.elements[8]?.elements[0]?.text,
-                      gpsY : data[index]?.elements[9]?.elements[0]?.text,
-                    },
-                  }}
-              style={{textDecoration:"none"}}>
-              <div className='image_container'>
-                <img src={image} alt={`Image ${index}`} />
-              </div>
-              <p style={{color:'black'}}>{data[index].elements[1].elements[0].text}</p>
-            </Link>
-          </div>
-        ))}
+          {thumbnails.map((image, index) => (
+            <div key={index} className="grid-item">
+              <Link
+                to={{
+                  pathname: `/detail/${data[index]?.elements[0]?.elements[0]?.text}`,
+                  state: {
+                    seq: data[index]?.elements[0]?.elements[0]?.text,
+                    title: data[index]?.elements[1]?.elements[0]?.text,
+                    place: data[index]?.elements[4]?.elements[0]?.text,
+                    realmName: data[index]?.elements[5]?.elements[0]?.text,
+                    area: data[index]?.elements[6]?.elements[0]?.text,
+                    image: data[index]?.elements[7]?.elements[0]?.text,
+                    gpsX: data[index]?.elements[8]?.elements[0]?.text,
+                    gpsY: data[index]?.elements[9]?.elements[0]?.text,
+                  },
+                }}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="image_container">
+                  <img src={image} alt={`Image ${index}`} />
+                </div>
+                <p style={{ color: "black" }}>
+                  {data[index].elements[1].elements[0].text}
+                </p>
+              </Link>
+            </div>
+          ))}
         </div>
       )}
-      
     </div>
   );
 }
--- src/componentes/HomeComponent/ShowRecommend.js
+++ src/componentes/HomeComponent/ShowRecommend.js
@@ -1,24 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import './ShowRecommend.css';
-import FetchMyData from '../FetchMyData';
-import FetchGenreData from '../FetchGenreData';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./ShowRecommend.css";
+import FetchMyData from "../FetchMyData";
+import FetchGenreData from "../FetchGenreData";
 import { useSelector } from "react-redux";
 function ShowRecommend() {
   const [data, setData] = useState([]);
   const [thumbnails, setThumbnails] = useState([]);
-  const initialLocation = '서울'; // 이 값을 원하는 기본값으로 설정하세요.
+  const initialLocation = "서울"; // 이 값을 원하는 기본값으로 설정하세요.
   const location = useSelector((state) => state.send.value) || initialLocation;
-  const [NoData,setNoData]=useState(false);
+  const [NoData, setNoData] = useState(false);
   useEffect(() => {
     async function fetchData(code) {
       try {
         let fetchedData = await FetchGenreData({ code: "A000" });
-        const filteredData = fetchedData.filter(item => item.elements[6].elements[0].text === location);
-        if(filteredData.length===0){
+        const filteredData = fetchedData.filter(
+          (item) => item.elements[6].elements[0].text === location
+        );
+        if (filteredData.length === 0) {
           setNoData(true);
-        }
-        else{
+        } else {
           setNoData(false);
           const slicedData = filteredData.slice(0, 4);
           const thumbnailArray = slicedData.map((item) => {
@@ -28,50 +29,51 @@ function ShowRecommend() {
           setThumbnails(thumbnailArray);
           console.log("Show realmCode:", code);
           console.log("Show filterdata", filteredData);
-
         }
-        
-        
       } catch (error) {
-        console.error('Show Error fetching data:', error);
+        console.error("Show Error fetching data:", error);
         setNoData(true);
       }
     }
-  
+
     fetchData();
   }, [location]);
 
   return (
     <div className="item-box">
-      {NoData?(
+      {NoData ? (
         <p>해당 지역에 맞는 연극이 없습니다.</p>
-      ):(
+      ) : (
         <div className="grid-container">
-        {thumbnails.map((image, index) => (
-          <div key={index} className="grid-item">
-            <Link to={{
-                    pathname: `/detail/${data[index]?.elements[0]?.elements[0]?.text}`,
-                    state: {
-                      title: data[index]?.elements[1]?.elements[0]?.text,
-                      place : data[index]?.elements[4]?.elements[0]?.text,
-                      realmName: data[index]?.elements[5]?.elements[0]?.text,
-                      area : data[index]?.elements[6]?.elements[0]?.text,
-                      image: data[index]?.elements[7]?.elements[0]?.text,
-                      gpsX : data[index]?.elements[8]?.elements[0]?.text,
-                      gpsY : data[index]?.elements[9]?.elements[0]?.text,
-                    },
-                  }}
-           style={{textDecoration:"none"}}>
-              <div className='image_container'>
-                <img src={image} alt={`Image ${index}`} />
-              </div>
-              <p style={{color:'black'}}>{data[index].elements[1].elements[0].text}</p>
-            </Link>
-          </div>
-        ))}
+          {thumbnails.map((image, index) => (
+            <div key={index} className="grid-item">
+              <Link
+                to={{
+                  pathname: `/detail/${data[index]?.elements[0]?.elements[0]?.text}`,
+                  state: {
+                    seq: data[index]?.elements[0]?.elements[0]?.text,
+                    title: data[index]?.elements[1]?.elements[0]?.text,
+                    place: data[index]?.elements[4]?.elements[0]?.text,
+                    realmName: data[index]?.elements[5]?.elements[0]?.text,
+                    area: data[index]?.elements[6]?.elements[0]?.text,
+                    image: data[index]?.elements[7]?.elements[0]?.text,
+                    gpsX: data[index]?.elements[8]?.elements[0]?.text,
+                    gpsY: data[index]?.elements[9]?.elements[0]?.text,
+                  },
+                }}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="image_container">
+                  <img src={image} alt={`Image ${index}`} />
+                </div>
+                <p style={{ color: "black" }}>
+                  {data[index].elements[1].elements[0].text}
+                </p>
+              </Link>
+            </div>
+          ))}
         </div>
       )}
-      
     </div>
   );
 }
