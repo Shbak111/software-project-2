fed9400f7522252a0a2e5f4ba4173747dcc5f63a
Shbak <khbak111@naver.com>
2023-11-01 13:21:13 +0900

    fix: restaurant recommend 기본형 변경

--- src/componentes/TourComponent/ZoneBox.js
+++ src/componentes/TourComponent/ZoneBox.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from "react";
 import ZoneContent from "./ZoneContent";
 import FetchLocalData from "../FetchLocalData";
-import { Link } from "react-router-dom/cjs/react-router-dom";
 import { useSelector } from "react-redux";
 import MichelinSeoul from "./MichelinSeoul";
 function ZoneBox() {
   const [items, setItems] = useState([]);
-  const [data, setData] = useState("");
+  const [data, setData] = useState(null);
   const [tmp, setTmp] = useState(false);
+
   const location = useSelector((state) => state.send.value);
-  console.log(`ZonBox :${location}`);
+  //console.log(`ZonBox :${location}`);
   useEffect(() => {
     async function fetchData() {
-      const newItems = Array.from({ length: 10 }, (_, index) => index + 1);
-      setItems([...items, ...newItems]);
       let localData = await FetchLocalData({ local: location });
 
       if (!localData || localData.length === 0) {
         // If no data is found for the selected location, default to "서울"
         localData = await FetchLocalData({ local: "서울" });
       }
-      // }
+      if (localData.length > 10) {
+        const newItems = Array.from({ length: 6 }, (_, index) => index + 1);
+        setItems([items, ...newItems]);
+      } else {
+        const newItems = Array.from(
+          { length: localData.length - 1 },
+          (_, index) => index + 1
+        );
+        setItems([items, ...newItems]);
+      }
       setData(localData);
       setTmp(!tmp);
       console.log("Data fetch success!");
@@ -29,70 +36,24 @@ function ZoneBox() {
     fetchData();
   }, [location]);
 
-  // 백승훈 추가내용
-  // DetailMap에 추가된것은 지도에 카테고리 검색으로 마커를 찍어오는 것이고
-  // 거기서 착안해서 지정좌표에 대한 카테고리 검색으로 주위 음식점을 리스트로 받아오는 파트.
-  useEffect(() => {
-    console.log("음식점 찾기");
-    const { kakao } = window;
-
-    var places = new kakao.maps.services.Places();
-
-    var callback = function (result, status) {
-      if (status === kakao.maps.services.Status.OK) {
-        console.log("#######################");
-        console.log(result);
-      }
-    };
-    items.map((item, index) => {
-      // 음식점 코드 검색
-      places.categorySearch("FD6", callback, {
-        // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
-        location: new kakao.maps.LatLng(
-          data[index]?.elements[9]?.elements[0]?.text,
-          data[index]?.elements[8]?.elements[0]?.text
-        ),
-        size: 3,
-        page: 1,
-        //sort: kakao.maps.services.SortBy.DISTANCE,
-      });
-      console.log(data[index]?.elements[1]?.elements[0]?.text);
-    });
-  }, [tmp]);
-
   return (
     <div>
       {location === "서울" ? <MichelinSeoul /> : <div />}
       <div>
-        {items.length !== 0 ? (
-          items.map((item, index) => (
-            <div key={index} style={{ cursor: "pointer" }}>
-              <Link
-                to={{
-                  pathname: `/detail/${data[index]?.elements[0]?.elements[0]?.text}`,
-                  state: {
-                    seq: data[index]?.elements[0]?.elements[0]?.text,
-                    title: data[index]?.elements[1]?.elements[0]?.text,
-                    place: data[index]?.elements[4]?.elements[0]?.text,
-                    realmName: data[index]?.elements[5]?.elements[0]?.text,
-                    area: data[index]?.elements[6]?.elements[0]?.text,
-                    image: data[index]?.elements[7]?.elements[0]?.text,
-                    gpsX: data[index]?.elements[8]?.elements[0]?.text,
-                    gpsY: data[index]?.elements[9]?.elements[0]?.text,
-                  },
-                }}
-              >
-                <ZoneContent
-                  area={data[index]?.elements[6]?.elements[0]?.text}
-                  zoneTitle={data[index]?.elements[1]?.elements[0]?.text}
-                  imageURL0={data[index]?.elements[7]?.elements[0]?.text}
-                />
-              </Link>
-            </div>
-          ))
-        ) : (
-          <div>Loading...</div>
-        )}
+        {data !== null
+          ? items.map((item, index) =>
+              data[index] !== null ? (
+                <div key={index}>
+                  <ZoneContent
+                    area={data[index]?.elements[6]?.elements[0]?.text}
+                    zoneTitle={data[index]?.elements[1]?.elements[0]?.text}
+                    imageURL0={data[index]?.elements[7]?.elements[0]?.text}
+                    datas={data[index]}
+                  />
+                </div>
+              ) : null
+            )
+          : null}
       </div>
     </div>
   );
--- src/componentes/TourComponent/ZoneContent.js
+++ src/componentes/TourComponent/ZoneContent.js
@@ -1,18 +1,94 @@
-import React from 'react';
-import "../TourComponent/ZoneContent.css"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import "../TourComponent/ZoneContent.css";
 
-function ZoneContent({ area, zoneTitle, imageURL0 }) {
-    const contentText = zoneTitle
-    ? `${zoneTitle}`
-    : `Loading...`;
-    return (
-        <div className="zone-container">
-                {contentText}
-                <div className="zone-content"> 
-                    <img className="img-left" src={imageURL0} alt="Loading.."></img>
-                </div>
+function ZoneContent({ area, zoneTitle, imageURL0, datas }) {
+  const [restaurants, setRes] = useState([]);
+  const [data, setData] = useState(null);
+  const contentText = zoneTitle ? `${zoneTitle}` : `Loading...`;
+
+  // 백승훈 추가내용
+  // DetailMap에 추가된것은 지도에 카테고리 검색으로 마커를 찍어오는 것이고
+  // 거기서 착안해서 지정좌표에 대한 카테고리 검색으로 주위 음식점을 리스트로 받아오는 파트.
+  useEffect(() => {
+    //console.log("음식점 찾기");
+    const { kakao } = window;
+    var places = new kakao.maps.services.Places();
+
+    var callback = function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        console.log("datas: ", datas);
+        console.log("result: ", result);
+        setRes(result);
+      }
+    };
+
+    // 음식점 코드 검색
+    places.categorySearch("FD6", callback, {
+      // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
+      location: new kakao.maps.LatLng(
+        datas?.elements[9]?.elements[0]?.text,
+        datas?.elements[8]?.elements[0]?.text
+      ),
+      size: 3,
+      page: 1,
+      //sort: kakao.maps.services.SortBy.DISTANCE,
+    });
+  }, [datas]);
+
+  return (
+    <div className="zone-container">
+      <p>{contentText}</p>
+      <div className="zone-content">
+        <div>
+          <Link
+            to={{
+              pathname: `/detail/${datas?.elements[0]?.elements[0]?.text}`,
+              state: {
+                seq: datas?.elements[0]?.elements[0]?.text,
+                title: datas?.elements[1]?.elements[0]?.text,
+                place: datas?.elements[4]?.elements[0]?.text,
+                realmName: datas?.elements[5]?.elements[0]?.text,
+                area: datas?.elements[6]?.elements[0]?.text,
+                image: datas?.elements[7]?.elements[0]?.text,
+                gpsX: datas?.elements[8]?.elements[0]?.text,
+                gpsY: datas?.elements[9]?.elements[0]?.text,
+              },
+            }}
+          >
+            <img className="img-left" src={imageURL0} alt="Loading.."></img>
+          </Link>
+          <p></p>
+          <h3 className="truncate">{contentText}</h3>
         </div>
-    );
+
+        <div>
+          <img
+            src="https://via.placeholder.com/320x480"
+            alt="placeholder"
+          ></img>
+          <p></p>
+          <text>{restaurants[0]?.place_name}</text>
+        </div>
+        <div>
+          <img
+            src="https://via.placeholder.com/320x480"
+            alt="placeholder"
+          ></img>
+          <p></p>
+          <text>{restaurants[1]?.place_name}</text>
+        </div>
+        <div>
+          <img
+            src="https://via.placeholder.com/320x480"
+            alt="placeholder"
+          ></img>
+          <p></p>
+          <text>{restaurants[2]?.place_name}</text>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ZoneContent;
