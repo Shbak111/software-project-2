84dc13085ad4830d77fc29448d4ccac97b2870fb
Shbak <khbak111@naver.com>
2023-11-13 20:44:16 +0900

    add: 상세정보 css 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -24,20 +24,29 @@ function Detail() {
         <text className="place">,{place}</text>
         <h1 className="title">{title}</h1>
         <p className="realmName">{realmName}</p>
-        
       </div>
-      <div className="pm-info">
-        <div className="poster-info">
-          <img className="poster" src={image} alt="Loading..." />
-          {/* <DetailMap gpsX={gpsX} gpsY={gpsY} /> */}
-        </div>
-        <div className="map-info">
-          <DetailMap gpsX={gpsX} gpsY={gpsY} />
-        </div>
+
+      <div className="poster-info">
+        <img className="poster" src={image} alt="Loading..." />
+        {/* <DetailMap gpsX={gpsX} gpsY={gpsY} /> */}
       </div>
-      
+
+      <div className="map-info">
+        <h1 className="ment">
+          전시 관람후에 가보면 좋을 음식점을 소개해 드려요!!!
+        </h1>
+        <h3 className="ment">
+          근처에 이런 음식점들이 있네요~ 방문해 봐도 좋을것 같아요!
+        </h3>
+        <DetailMap gpsX={gpsX} gpsY={gpsY} />
+      </div>
+
       <div className="detail-review">
-          <DetailComment seq={seq} />
+        <h1 className="ment">이 전시를 관람하셨다면 후기를 남겨주세요!!!!</h1>
+        <h3 className="ment">
+          여러분의 소중한 의견은 다른분들에게 도움이 된답니다!
+        </h3>
+        <DetailComment seq={seq} />
       </div>
     </div>
   );
